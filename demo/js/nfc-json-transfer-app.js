fcfef79f41cbcef288679815c04a695198492ecb
Espen Klem <espen.klem@gmail.com>
2022-11-20 16:17:29 +0100

    demo scaffolding Most of demo scaffolding added. Ready to test NFC code

---
+++ demo/js/nfc-json-transfer-app.js
@@ -0,0 +1,8 @@
+// Listen to key up on headlinetext and initiate a daq-proc
+document.getElementById('readButton').onclick = function () {
+  popHTML('read-button clicked', { tagToPop: 'console', tagToPopWith: 'div', append: true })
+}
+
+document.getElementById('writeButton').onclick = function () {
+  popHTML('write-button clicked', { tagToPop: 'console', tagToPopWith: 'div', append: true })
+}
