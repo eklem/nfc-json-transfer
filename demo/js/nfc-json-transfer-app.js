25966d2f97af553c185fe90ce0617448ebc92818
Espen Klem <espen.klem@gmail.com>
2022-11-28 19:38:16 +0100

    Something working Reading and writing works, kind of. Doesn't manage to show the actual message that was written & event listeners needs to be turned off again: https://github.com/eklem/nfc-json-transfer/issues/9

--- demo/js/nfc-json-transfer-app.js
+++ demo/js/nfc-json-transfer-app.js
@@ -1,7 +1,45 @@
-document.getElementById('readButton').onclick = function () {
-  popHTML('read-button clicked', { tagToPop: 'console', tagToPopWith: 'div', append: true })
-}
+// document.getElementById('readButton').onclick = function () {
+//   popHTML('read-button clicked', { tagToPop: 'console', tagToPopWith: 'div', append: true })
+// }
 
-document.getElementById('writeButton').onclick = function () {
-  popHTML('write-button clicked', { tagToPop: 'console', tagToPopWith: 'div', append: true })
-}
+// document.getElementById('writeButton').onclick = function () {
+//   popHTML('write-button clicked', { tagToPop: 'console', tagToPopWith: 'div', append: true })
+// }
+
+readButton.addEventListener("click", async () => {
+  console.log("User clicked scan button")
+  try {
+    const ndef = new NDEFReader()
+    await ndef.scan()
+    console.log("> Scan started")
+
+    ndef.addEventListener("readingerror", () => {
+      console.log("Argh! Cannot read data from the NFC tag. Try another one?")
+    })
+
+    ndef.addEventListener("reading", ({ message, serialNumber }) => {
+      console.log(`> Serial Number: ${serialNumber}`)
+      console.log(`> Records: (${message.records.length})`)
+      const msg = 'Read<br/>Serial number: ' + serialNumber + '<br>Records: ' + message.records.length + '<br>message: ' + message
+      popHTML(msg, { tagToPop: 'console', tagToPopWith: 'div', append: true })
+    })
+  } catch (error) {
+    console.log('Error reading: ' + error)
+    const msg = 'Read<br/>Error reading: ' + error
+    popHTML(msg, { tagToPop: 'console', tagToPopWith: 'div', append: true })
+  }
+})
+
+writeButton.addEventListener("click", async () => {
+  console.log("User clicked write button")
+  try {
+    const ndef = new NDEFReader()
+    await ndef.write('Hello world!')
+    console.log('> Message written')
+    popHTML('write-button clicked', { tagToPop: 'console', tagToPopWith: 'div', append: true })
+  } catch (error) {
+    console.log('Error writing ' + error)
+    const msg = 'Write<br/>Error writing:  ' + error
+    popHTML(msg, { tagToPop: 'console', tagToPopWith: 'div', append: true })
+  }
+})
