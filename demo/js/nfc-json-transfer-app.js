26bf0566f4a8de364f2637402772444fe883e57c
Espen Klem <espen.klem@gmail.com>
2022-11-30 00:15:21 +0100

    some JSON writing and reading working

--- demo/js/nfc-json-transfer-app.js
+++ demo/js/nfc-json-transfer-app.js
@@ -6,23 +6,23 @@
 //   popHTML('write-button clicked', { tagToPop: 'console', tagToPopWith: 'div', append: true })
 // }
 
+const ndef = new NDEFReader();
+
 readButton.addEventListener("click", async () => {
-  console.log("User clicked scan button")
   try {
-    const ndef = new NDEFReader()
-    await ndef.scan()
-    console.log("> Scan started")
-
-    ndef.addEventListener("readingerror", () => {
-      console.log("Argh! Cannot read data from the NFC tag. Try another one?")
-    })
-
-    ndef.addEventListener("reading", ({ message, serialNumber }) => {
-      console.log(`> Serial Number: ${serialNumber}`)
-      console.log(`> Records: (${message.records.length})`)
-      const msg = 'read<br/>Serial number: ' + serialNumber + '<br>Records: ' + message.records.length + '<br>message: ' + message
-      popHTML(msg, { tagToPop: 'console', tagToPopWith: 'div', append: true })
-    })
+    await ndef.scan();
+    ndef.onreading = (event) => {
+      const decoder = new TextDecoder()
+      for (const record of event.message.records) {
+        if (record.mediaType === "application/json") {
+          const json = JSON.parse(decoder.decode(record.data))
+          const article =/^[aeio]/i.test(json.title) ? "an" : "a"
+          console.log(`${json.name} is ${article} ${json.title}`)
+          const msg = 'read<br/>' + json.name + ' is ' + article + ' ' + json.title
+          popHTML(msg, { tagToPop: 'console', tagToPopWith: 'div', append: true })
+        }
+      }
+    }
   } catch (error) {
     console.log('Error reading: ' + error)
     const msg = 'read<br/>Error reading: ' + error
@@ -31,11 +31,28 @@ readButton.addEventListener("click", async () => {
 })
 
 writeButton.addEventListener("click", async () => {
-  console.log("User clicked write button")
   try {
-    const ndef = new NDEFReader()
-    await ndef.write('Hello world!')
-    console.log('> Message written')
+    const encoder = new TextEncoder();
+    await ndef.write({
+      records: [
+        {
+          recordType: "mime",
+          mediaType: "application/json",
+          data: encoder.encode(JSON.stringify({
+            name: "Benny Jensen",
+            title: "Banker"
+          }))
+        },
+        {
+          recordType: "mime",
+          mediaType: "application/json",
+          data: encoder.encode(JSON.stringify({
+            name: "Zoey Braun",
+            title: "Engineer"
+          }))
+        }
+      ]
+    })
     popHTML('write', { tagToPop: 'console', tagToPopWith: 'div', append: true })
   } catch (error) {
     console.log('Error writing ' + error)
