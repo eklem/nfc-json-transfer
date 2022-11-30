2ef705c70a2281a8afa1eb3289c55b68b616960e
Espen Klem <espen.klem@gmail.com>
2022-11-30 11:11:57 +0100

    Debugging with eruda

--- demo/js/nfc-json-transfer-app.js
+++ demo/js/nfc-json-transfer-app.js
@@ -6,11 +6,11 @@
 //   popHTML('write-button clicked', { tagToPop: 'console', tagToPopWith: 'div', append: true })
 // }
 
-const ndef = new NDEFReader();
+const ndef = new NDEFReader()
 
-readButton.addEventListener("click", async () => {
+readButton.addEventListener('click', async () => {
   try {
-    await ndef.scan();
+    await ndef.scan()
     ndef.onreading = (event) => {
       const decoder = new TextDecoder()
       const records = event.message.records
@@ -19,11 +19,11 @@ readButton.addEventListener("click", async () => {
       for (const record of event.message.records) {
         const soFar = 'Got inside the for-loop'
         popHTML(soFar, { tagToPop: 'console', tagToPopWith: 'div', append: true })
-        if (record.mediaType === "application/json") {
+        if (record.mediaType === 'application/json') {
           const isJson = 'record is JSON data'
           popHTML(isJson, { tagToPop: 'console', tagToPopWith: 'div', append: true })
-          const json = decoder.decode(record.data)
-          const msg = 'Read</br>' + json
+          const jsonString = decoder.decode(record.data)
+          const msg = 'Read</br>' + jsonString
           popHTML(msg, { tagToPop: 'console', tagToPopWith: 'div', append: true })
         } else {
           const notJson = 'Read</br>Message not JSON.'
@@ -38,25 +38,25 @@ readButton.addEventListener("click", async () => {
   }
 })
 
-writeButton.addEventListener("click", async () => {
+writeButton.addEventListener('click', async () => {
   try {
-    const encoder = new TextEncoder();
+    const encoder = new TextEncoder()
     const data = [
       {
-        name: "Benny Jensen",
-        title: "Banker"
+        name: 'Benny Jensen',
+        title: 'Banker'
       },
       {
-        name: "Zoey Braun",
-        title: "Engineer"
+        name: 'Zoey Braun',
+        title: 'Engineer'
       }
     ]
     const dataString = JSON.stringify(data)
     await ndef.write({
       records: [
         {
-          recordType: "mime",
-          mediaType: "application/json",
+          recordType: 'mime',
+          mediaType: 'application/json',
           data: encoder.encode(dataString)
         }
       ]
